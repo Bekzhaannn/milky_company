eff57b4feb5df0cde6608ad3b008e88cadc62155
bekzhaannn <lllbekzhan@gmail.com>
2023-05-21 12:33:01 +0600

    milky

--- index.py
+++ index.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', data=data)
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
